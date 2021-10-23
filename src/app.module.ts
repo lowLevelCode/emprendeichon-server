3a1dfd0570be85a02755c355b60616cbce1cebdb
jesus raul alarcon <jesus@insaite.io>
2021-10-23 03:28:20 -0600

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { EchoModule } from './echo/echo.module';
+
+@Module({
+  imports: [EchoModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
